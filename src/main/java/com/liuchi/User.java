c960cd837fa701232bb889d46ba3f28ad6f5e9d9
unknown <liuchi@rong360ad.com>
2018-03-16 20:40:28 +0800

    test

---
+++ src/main/java/com/liuchi/User.java
@@ -0,0 +1,22 @@
+package com.liuchi;
+
+/**
+ * @author liuchi
+ */
+public class User {
+    private String name;
+    private int id;
+    public User(String myName, int  myid) {
+        name = myName;
+        id = myid;
+    }
+    public void eat(){
+        System.out.println(name+"正在吃");
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction() {
+        System.out.println("大家好！我是"         + id + "号" + name + ".");
+    }
+}
